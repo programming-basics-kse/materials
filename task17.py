9a631460d3971dfb2f8a32d128006017c8bcedc8
Anna Opanasenko <aopanasenko@kse.org.ua>
2023-11-23 13:23:27 +0100

    Create task17.py

---
+++ task17.py
@@ -0,0 +1,8 @@
+array1 = input("First array: ")
+array2 = input("Second array: ")
+
+if len(array1) > len(array2):
+    print("array1 is bigger")
+elif len(array2) > len(array1):
+    print("array2 is bigger")
+
